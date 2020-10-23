8f8ea3cbf77d4e1f4506ba771dfb7f98b3e6d7f0
yash3001 <yashchauhan197@gmail.com>
2020-10-23 14:39:32 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import logo from './logo.png'
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  state = [];
+
+  render() {
+    return(
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Coming Soon!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/c/mccoders/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Youtube
         </a>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
