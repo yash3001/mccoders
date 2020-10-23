b99050816ce166382ccd0cec1cae61fc538c960a
yash3001 <yashchauhan197@gmail.com>
2020-10-23 14:03:26 +0530

    Updates

---
+++ precache-manifest.7c2f2410b12d9aa51b336d9d87e03d50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ce558cd7b3bf9e4977db91123cf34b",
+    "url": "/mccoders/index.html"
+  },
+  {
+    "revision": "ee6a538e103b343f2247",
+    "url": "/mccoders/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6f45ca51b09bcefcfc21",
+    "url": "/mccoders/static/js/2.be828830.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/mccoders/static/js/2.be828830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee6a538e103b343f2247",
+    "url": "/mccoders/static/js/main.36a493f6.chunk.js"
+  },
+  {
+    "revision": "555d635fa6100f60d820",
+    "url": "/mccoders/static/js/runtime-main.6abdee94.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mccoders/static/media/logo.5d5d9eef.svg"
+  }
+]);
